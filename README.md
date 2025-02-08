3199e9a00d69328c4415f71259d8f4312f8912d5
jonasrsv42 <jonasrsv42@gmail.com>
2025-02-08 09:56:46 +0100

    Fixed Bug</p>

--- README.md
+++ README.md
@@ -1,6 +1,89 @@
 
 # Areamy
 
-Areamy (Audio Streamy) is a runtime for streaming graphs in rust. 
-
+Areamy is a strongly typed runtime for multithreaded streaming graphs. 
 See [src/connect/graph](src/connect/graph.rs) for a brief overview.
+
+It serves a purpose similar to https://github.com/google-ai-edge/mediapipe
+
+The areamy repository itself only has the basic building blocks of the runtime.  
+
+
+## Example
+
+
+See [tests/example.rs](tests/example.rs)
+
+
+```rust
+
+let mut in_node = areamy::sync::make_line(Adder::new())?;
+let mut middle_node = areamy::sync::make_line(Adder::new())?;
+let mut out_node = areamy::sync::make_line(Adder::new())?;
+
+let source = areamy::sync::Source::<usize>::of(in_node.clone())?;
+
+areamy::sync::Connect::<usize>::bidi(&mut in_node, &mut middle_node)?;
+areamy::sync::Connect::<usize>::bidi(&mut middle_node, &mut out_node)?;
+
+let sink = areamy::sync::Sink::new(out_node.workable(), out_node.output())?;
+
+let mut reader = areamy::LineReader::new(source, sink);
+
+reader.push(areamy::Message::Data(1))?;
+reader.push(areamy::Message::Data(2))?;
+
+assert_eq!(reader.read().unwrap(), areamy::Message::Data(4));
+assert_eq!(reader.read().unwrap(), areamy::Message::Data(5));
+
+
+```
+
+The purpose of areamy is to support multithreaded graphs such as the example below. Where 
+there are two threads working on the graph. The main thread and a `HelperThread`. In the example below
+
+```
+ main thread
+     ↑
+    node
+     ↑
+    node   (helper_thread)
+     ↑
+    node   (helper_thread)
+
+```
+
+the graph computation is split across two threads.
+
+```rust
+
+let mut in_node = areamy::sync::make_line(Adder::new())?;
+let mut middle_node = areamy::sync::make_line(Adder::new())?;
+let mut out_node = areamy::sync::make_line(Adder::new())?;
+
+let source = areamy::sync::Source::<usize>::of(in_node.clone())?;
+areamy::sync::Connect::<usize>::bidi(&mut in_node, &mut middle_node)?;
+
+let mut helper_thread = areamy::ThreadStream::<HelperThread>::new();
+
+// Now helper thread will work on the middle_node subgraph.
+areamy::make_work(&middle_node, helper_thread.as_mut())?;
+
+// Ensure that middle node, using the `HelperThread` pushes data into out node.
+areamy::sync::Connect::<usize>::push(&mut middle_node, &out_node)?;
+
+let sink = areamy::sync::Sink::new(out_node.workable(), out_node.output())?;
+let mut reader = areamy::LineReader::new(source, sink);
+
+// Start the helper thread.
+helper_thread.start()?;
+
+// Helper thread will run the computation in the first two nodes.
+reader.push(areamy::Message::Data(1))?;
+reader.push(areamy::Message::Data(2))?;
+
+// Main thread runs computation in the final output node.
+assert_eq!(reader.read().unwrap(), areamy::Message::Data(4));
+assert_eq!(reader.read().unwrap(), areamy::Message::Data(5));
+
+```
---
+++ tests/example.rs
@@ -0,0 +1,119 @@
+use areamy;
+
+use std::collections::VecDeque;
+
+pub struct Adder {
+    output: VecDeque<usize>,
+}
+
+impl Adder {
+    pub fn new() -> Result<Self, areamy::error::Error> {
+        Ok(Self {
+            output: VecDeque::new(),
+        })
+    }
+}
+
+impl areamy::LineRoutine<usize, usize> for Adder {
+    fn output(&mut self) -> &mut VecDeque<usize> {
+        &mut self.output
+    }
+
+    fn work(&mut self, message: usize) -> Result<(), areamy::error::Error> {
+        Ok(self.output.push_back(message + 1))
+    }
+
+    fn flush(&mut self) -> Result<(), areamy::error::Error> {
+        Ok(())
+    }
+}
+
+#[test]
+fn simple_sync() -> Result<(), areamy::error::Error> {
+    let mut in_node = areamy::sync::make_line(Adder::new())?;
+    let mut middle_node = areamy::sync::make_line(Adder::new())?;
+    let mut out_node = areamy::sync::make_line(Adder::new())?;
+
+    let source = areamy::sync::Source::<usize>::of(in_node.clone())?;
+
+    areamy::sync::Connect::<usize>::bidi(&mut in_node, &mut middle_node)?;
+    areamy::sync::Connect::<usize>::bidi(&mut middle_node, &mut out_node)?;
+
+    let sink = areamy::sync::Sink::new(out_node.workable(), out_node.output())?;
+
+    let mut reader = areamy::LineReader::new(source, sink);
+
+    reader.push(areamy::Message::Data(1))?;
+    reader.push(areamy::Message::Data(2))?;
+
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(4));
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(5));
+
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+struct HelperThread {}
+
+impl areamy::ThreadId for HelperThread {}
+
+#[test]
+fn sync_multithread() -> Result<(), areamy::error::Error> {
+    // Example of multithreaded graph.
+
+    let mut in_node = areamy::sync::make_line(Adder::new())?;
+    let mut middle_node = areamy::sync::make_line(Adder::new())?;
+    let mut out_node = areamy::sync::make_line(Adder::new())?;
+
+    let source = areamy::sync::Source::<usize>::of(in_node.clone())?;
+    areamy::sync::Connect::<usize>::bidi(&mut in_node, &mut middle_node)?;
+
+    let mut helper_thread = areamy::ThreadStream::<HelperThread>::new();
+
+    // Now helper thread will work on the middle_node subgraph.
+    areamy::make_work(&middle_node, helper_thread.as_mut())?;
+
+    // Ensure that middle node, using the `HelperThread` pushes data into out node.
+    areamy::sync::Connect::<usize>::push(&mut middle_node, &out_node)?;
+
+    let sink = areamy::sync::Sink::new(out_node.workable(), out_node.output())?;
+    let mut reader = areamy::LineReader::new(source, sink);
+
+    // Start the helper thread.
+    helper_thread.start()?;
+
+    // Helper thread will run the computation in the first two nodes.
+    reader.push(areamy::Message::Data(1))?;
+    reader.push(areamy::Message::Data(2))?;
+
+    // Main thread runs computation in the final output node.
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(4));
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(5));
+
+    Ok(())
+}
+
+#[test]
+fn simple_nosync() -> Result<(), areamy::error::Error> {
+    // Nosync is useful to avoid unnecessary mutexes.
+    // each connection is lockfree, at the cost of not being `Sync`.
+
+    let in_node = areamy::nosync::root(Adder::new())?;
+
+    let source = areamy::sync::Source::<usize>::of(in_node.input.clone())?;
+
+    let middle_node = areamy::nosync::Connect::<usize>::pull(in_node, Adder::new())?;
+    let out_node = areamy::nosync::Connect::<usize>::pull(middle_node, Adder::new())?;
+
+    let sink = areamy::nosync::Sink::new(out_node);
+
+    let mut reader = areamy::LineReader::new(source, sink);
+
+    reader.push(areamy::Message::Data(1))?;
+    reader.push(areamy::Message::Data(2))?;
+
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(4));
+    assert_eq!(reader.read().unwrap(), areamy::Message::Data(5));
+
+    Ok(())
+}

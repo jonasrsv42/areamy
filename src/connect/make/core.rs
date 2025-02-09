84d3e0c22dd4606f0b8e8639162f9214a5e59951
jonasrsv42 <jonasrsv42@gmail.com>
2025-02-09 16:17:55 +0100

    _</p>

--- src/connect/make/core.rs
+++ src/connect/make/core.rs
@@ -5,33 +5,32 @@ use crate::{
     Pushable, Workable,
 };
 
-/// A bidi is a both-way connection between streaming nodes. The ChildType
-/// will lend its thread to the parent by working it, and the parent will
-/// push data into the childs pushable when available, then return the thread.
+/// `make_bidi` creates a `bidi` connection between two nodes.
 ///
-/// bidi can only connect nodes where the parent has a single output
-/// and the child a single input. For more complicated nodes
-/// such as connecting to a `bifurcation` output or
-/// `biunion` input one should use  `make_push` and `make_work` directly.
+/// A `bidi` (bidirectional) is a connection where data flows from parent to child and scheduling
+/// from child to parent.
+///
+/// The child lends its `ThreadIdType` to the parent for it to `work` and then `push` the `MessageType`
+/// back into the child.
+///
+/// * `parent` - A type that we can retrieve a (Workable) from and add a (Pushable) into. The
+///     (Workable) will be added to the child so the child can schedule the work. The (Pushable)
+///     will be added as output for the child so it can push into the parent. The method takes
+///     a (&mut) because the (Add) method requires a mutable reference.
+///
+/// * `child` - A type that we can add the parent (Workable) into and from which we can retrieve the
+///     (Pushable) and give to the parent.
 pub fn make_bidi<
-    ParentType,                       // Generic over parent node
-    ChildType,                        // Generic over child node
-    ChildMultiplicity: Multiplicity,  // Generic over type of child connection
+    ChildMultiplicity: Multiplicity, // Generic over type of child connection
     ParentMultiplicity: Multiplicity, // Generic over type of parent connection
     MessageType,
     ThreadIdType,
 >(
-    parent: &mut ParentType,
-    child: &mut ChildType,
-) -> Result<(), Error>
-where
-    ChildType: 'static
-        + Add<dyn Workable<ThreadId = ThreadIdType>, ChildMultiplicity>
-        + Get<dyn Pushable<Message = MessageType>, ChildMultiplicity>,
-    ParentType: 'static
-        + Add<dyn Pushable<Message = MessageType>, ParentMultiplicity>
-        + Get<dyn Workable<ThreadId = ThreadIdType>, ParentMultiplicity>,
-{
+    parent: &mut (impl Add<dyn Pushable<Message = MessageType>, ParentMultiplicity>
+              + Get<dyn Workable<ThreadId = ThreadIdType>, ParentMultiplicity>),
+    child: &mut (impl Add<dyn Workable<ThreadId = ThreadIdType>, ChildMultiplicity>
+              + Get<dyn Pushable<Message = MessageType>, ChildMultiplicity>),
+) -> Result<(), Error> {
     make_push(parent, child)?;
     make_work(parent, child)?;
 

5976506c0823d2f8f0ea210975223ba163204b55
jonasrsv42 <jonasrsv42@gmail.com>
2026-04-06 08:56:40 +0200

    Another bug bites the dust</p>

--- src/poll/mod.rs
+++ src/poll/mod.rs
@@ -5,6 +5,7 @@ pub mod join;
 pub mod select;
 
 pub use crate::connect::poll::edge::{Async, AsyncIn, Deferred, Edge, Linktime, Null, Sync};
+pub use crate::node::line::poll::factory::PollLineRoutineFactory;
 pub use future::{FutureRoutine, Input, InputConsumer, OutputProducer};
 pub use join::Join;
 pub use select::Select;
